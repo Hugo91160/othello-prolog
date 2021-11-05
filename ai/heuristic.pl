3112823e4b743ddb019f3a3adef0f97a763c485f
Hugo91160 <73596098+Hugo91160@users.noreply.github.com>
2021-11-05 12:13:59 +0100

    Update heuristic.pl

--- ai/heuristic.pl
+++ ai/heuristic.pl
@@ -24,11 +24,11 @@
   % between the max player and the min player.
   % The return value is determined as follows :
 
-coinParityHeuristic(Grid, MaxPlayer, MinPlayer, Res) :-
-  gridToLine(Grid, AsLine),
-  nb_elem(AsLine, Nb_MaxCoins, MaxPlayer),
-  nb_elem(AsLine, Nb_MinCoins, MinPlayer),
-  Res is 100 * (Nb_MaxCoins - Nb_MinCoins) / (Nb_MaxCoins + Nb_MinCoins).
+coinParityHeuristic(Grille, MaxPlayer, MinPlayer, Res) :-
+  grilleEnLigne(Grille, GrilleLigne),
+  nb_elements(GrilleLigne, MaxPlayer, NbCoinMaxPlayer),
+  nb_elements(GrilleLigne, MinPlayer, NbCoinMinPlayer),
+  Res is 100 * (NbCoinMaxPlayer - NbCoinMinPlayer) / (NbCoinMaxPlayer + NbCoinMinPlayer).
 
 
       % grilleDeDepart(Grille), coinParityHeuristic(Grille, x, o, 0). % test Match (start of game == even == 0)
@@ -41,35 +41,32 @@ coinParityHeuristic(Grid, MaxPlayer, MinPlayer, Res) :-
   % and increasing one’s own mobility.
   % This value is calculated as follows :
 
-mobilityHeuristic(Grid, MaxPlayer, MinPlayer, Res) :-
-  allValidMove(MaxPlayer, Grid, MovesMax), length(MovesMax, Nb_MaxMoves),
-  allValidMove(MinPlayer, Grid, MovesMin), length(MovesMin, Nb_MinMoves),
-
-  Nb_AllMoves is Nb_MaxMoves + Nb_MinMoves,
-
-                                           % @drakirus tweak:
-  ( Nb_MinMoves =:= 0, Nb_AllMoves =\= 1   % check if not end of game and
-    /* Then */ ->  Res = 999999            % if you can make the other player skip his turn bonus points
-
-      /* Else */
-    ;( Nb_MaxMoves =:= 0, Nb_AllMoves =\= 1
-        /* Then */ -> Res = -999999
-
-      /* Else */ ;
-      ( /* If */  Nb_AllMoves =:= 0
-        /* Then */ ->  Res = 0
-        /* Else */ ; Res is 100 * (Nb_MaxMoves - Nb_MinMoves) / (Nb_MaxMoves + Nb_MinMoves)
-      )
-
-    )
-
-  ).
-
+mobilityHeuristic(Grille, MaxPlayer, MinPlayer, Mobility) :-
+  
       % grilleDeDepart(Grille), mobilityHeuristic(Grille, x, o, 0). % test Match
 
       % grilleDeDepart([L1|Rest]), Block = ["-", "-", "-", "-", "-", o, o, x],  GrilleBlock= [L1, L1, L1, L1, L1, Block, Block, Block],
       % afficheGrille(GrilleBlock),
       % mobilityHeuristic(GrilleBlock, x, o, X),!. %test Trun Blocked heuristic
+    allValidMove(MaxPlayer, Grille, AllMovesMaxPlayer), 
+    length(AllMovesMaxPlayer, Nb_MaxMoves),
+    allValidMove(MinPlayer, Grille, AllMovesMinPlayer), 
+    length(AllMovesMinPlayer, Nb_MinMoves),
+
+    /*Formula for the mobility heuristic : 
+    if((Max Player Actual Mobility Value + Min Player Actual Mobility 
+        Value) !=0) 
+        Actual Mobility Heuristic Value =   100* (Max Player Actual Mobility Value –Min Player Actual 
+        Mobility Value)/ (Max Player Actual Mobility Value + Min Player Actual Mobility Value)  
+    else 
+        Actual Mobility Heuristic Value = 0
+*/
+    Sum is Nb_MaxMoves + Nb_MinMoves,
+    ( Sum \= 0 -> %if
+        Mobility is 100 * (Nb_MaxMoves - Nb_MinMoves) / (Nb_MaxMoves + Nb_MinMoves) ; %then
+        Mobility is 0 %else
+    ).
+
 
 
 
@@ -82,13 +79,22 @@ mobilityHeuristic(Grid, MaxPlayer, MinPlayer, Res) :-
   % This value is captured as follows :
 
 cornersCapturedHeuristic(Grid, MaxPlayer, MinPlayer, Res) :-
+/*Formula : 
+if((Max Player Corner Value + Min Player Corner Value) !=0) 
+  Corner Heuristic Value =  
+  100* (Max Player Corner Heurisitc Value –Min Player Corner 
+  Heuristic Value)/ 
+  (Max Player Corner Heuristic Value + Min Player Corner Heurisitc Value)  
+else 
+  Corner Heuristic Value = 0 
+*/
   getAllCorners(Grid, AsLine),
-  nb_elem(AsLine, Nb_MaxCoins, MaxPlayer),
-  nb_elem(AsLine, Nb_MinCoins, MinPlayer),
+  nb_elements(AsLine, Nb_MaxCoins, MaxPlayer),
+  nb_elements(AsLine, Nb_MinCoins, MinPlayer),
   Nb_AllCorners is Nb_MaxCoins + Nb_MinCoins,
-    ( /* If */  Nb_AllCorners =:= 0
-      /* Then */ ->  Res = 0
-      /* Else */ ; Res is 100 * (Nb_MaxCoins - Nb_MinCoins) / (Nb_MaxCoins + Nb_MinCoins)
+    ( /* If */  Nb_AllCorners \= 0
+      /* Then */ ->  Res is 100 * (Nb_MaxCoins - Nb_MinCoins) / (Nb_MaxCoins + Nb_MinCoins)
+      /* Else */ ; Res is 0
      ).
 
      % grilleDeDepart(Grille), cornersCapturedHeuristic(Grille, x, o, 0). % test Match
@@ -110,67 +116,141 @@ cornersCapturedHeuristic(Grid, MaxPlayer, MinPlayer, Res) :-
   % Corners are always stable in nature, and by building upon corners,
   % more coins become stable in the region.
 
-  % The stability value is calculated as follows :
+  % Tab which classify the coins :
+  /*
+  1 : stable
+  0 : semi-stable 
+  -1 : unstable
+  */
+stabilityHeuristic(Grid, MaxPlayer, MinPlayer, Res_stability) :- 
+  nbCaseStable(Grid, MaxPlayer, StableMaxPlayer),
+  nbCaseStable(Grid, MinPlayer, StableMinPlayer),
 
-stability_weights([4,  -3,  2,  2,  2,  2, -3,  4,
-                   -3, -4, -1, -1, -1, -1, -4, -3,
-                   2,  -1,  1,  0,  0,  1, -1,  2,
-                   2,  -1,  0,  1,  1,  0, -1,  2,
-                   2,  -1,  0,  1,  1,  0, -1,  2,
-                   2,  -1,  1,  0,  0,  1, -1,  2,
-                   -3, -4, -1, -1, -1, -1, -4, -3,
-                   4,  -3,  2,  2,  2,  2, -3,  4]).
+  nbCaseInstable(Grid, MaxPlayer, InstableMaxPlayer),
+  nbCaseInstable(Grid, MinPlayer, InstableMinPlayer),
+  StabilityMaxPlayer is StableMaxPlayer - InstableMaxPlayer,
+  StabilityMinPlayer is StableMinPlayer - InstableMinPlayer,
+
+  Sum is StabilityMaxPlayer + StabilityMinPlayer,
+  (
+    Sum \= 0 
+    -> Res_stability is 100 * (StabilityMaxPlayer - StabilityMinPlayer) / (StabilityMaxPlayer + StabilityMinPlayer)
+    ; Res_stability is 0
+  ).
+
+
+
+
+
+
+
+
+
+
+
+
+%% Static evaluation
+static_weights_heuristic_function([4,  -3,  2,  2,  2,  2, -3,  4,
+                                  -3, -4, -1, -1, -1, -1, -4, -3,
+                                  2,  -1,  1,  0,  0,  1, -1,  2,
+                                  2,  -1,  0,  1,  1,  0, -1,  2,
+                                  2,  -1,  0,  1,  1,  0, -1,  2,
+                                  2,  -1,  1,  0,  0,  1, -1,  2,
+                                  -3, -4, -1, -1, -1, -1, -4, -3,
+                                  4,  -3,  2,  2,  2,  2, -3,  4]).
 
 % encapsulate
-stabilityHeuristic(Grid, MaxPlayer, MinPlayer, Res) :-
-  gridToLine(Grid, AsLine),
-  stability_weights(Stability_line),
-  stabilityHeuristic_CB(AsLine, Stability_line,
+static_heuristic_evaluation(Grid, MaxPlayer, MinPlayer, Res) :-
+  grilleEnLigne(Grid, AsLine),
+  static_weights_heuristic_function(Stability_line),
+  static_heuristic_evaluation_CB(AsLine, Stability_line,
                         MaxPlayer, MinPlayer,
                         Res_PlayerMax, Res_PlayerMin),
 
   Res is Res_PlayerMax - Res_PlayerMin.
 
-stabilityHeuristic_CB([], [], _, _, 0, 0).
+static_heuristic_evaluation_CB([], [], _, _, 0, 0).
 
-stabilityHeuristic_CB([Head_grid|Tail_grid], [Head_weights|Tail_weights],
+static_heuristic_evaluation_CB([Head_grid|Tail_grid], [Head_weights|Tail_weights],
                       /* MaxPlayer = */ Head_grid, MinPlayer,
                       Res_PlayerMax, Res_PlayerMin) :-
 
-  stabilityHeuristic_CB(Tail_grid, Tail_weights,
+  static_heuristic_evaluation_CB(Tail_grid, Tail_weights,
                      Head_grid, MinPlayer,
                      Tmp_ResPlayerMax, Res_PlayerMin),!,
 
   Res_PlayerMax is Tmp_ResPlayerMax + Head_weights.
 
-stabilityHeuristic_CB([Head_grid|Tail_grid], [Head_weights|Tail_weights],
+static_heuristic_evaluation_CB([Head_grid|Tail_grid], [Head_weights|Tail_weights],
                       MaxPlayer, /* MinPlayer = */ Head_grid,
                       Res_PlayerMax, Res_PlayerMin) :-
 
-  stabilityHeuristic_CB(Tail_grid, Tail_weights,
+  static_heuristic_evaluation_CB(Tail_grid, Tail_weights,
                      MaxPlayer, Head_grid,
                      Res_PlayerMax, Tmp_ResPlayerMin),!,
 
   Res_PlayerMin is Tmp_ResPlayerMin + Head_weights.
 
-stabilityHeuristic_CB([_|TG], [_|TW], MaxPlayer, MinPlayer, ResMax, ResMin) :-
-  stabilityHeuristic_CB(TG, TW, MaxPlayer, MinPlayer, ResMax, ResMin).
+static_heuristic_evaluation_CB([_|TG], [_|TW], MaxPlayer, MinPlayer, ResMax, ResMin) :-
+  static_heuristic_evaluation_CB(TG, TW, MaxPlayer, MinPlayer, ResMax, ResMin).
 
       % grilleDeDepart(Grid),
-      % stabilityHeuristic(Grid, x, o, 0). % test Match
+      % static_heuristic_evaluation(Grid, x, o, 0). % test Match
 
 
 
   %% Sum of all previous heuristic with their respective weights %%
 
-dynamic_heuristic_evaluation(Grid, MaxPlayer, MinPlayer, Res) :-
+
+
+
+%% 
+getWeight(Grid, Res_corners, Res_mobility, Res_coinParity, Res_stability, Res) :- 
+  count_xo_grid(Count, Grid),
+  Count =< 20,
+  Res is 100 * Res_corners + 10 * Res_mobility + 5 * Res_coinParity + 50 * Res_stability.
+
+getWeight(Grid, Res_corners, Res_mobility, Res_coinParity, Res_stability, Res) :- 
+  count_xo_grid(Count, Grid),
+  Count > 20,
+  Count =< 50,
+  Res is 100 * Res_corners + 50 * Res_mobility + 10 * Res_coinParity + 25 * Res_stability.
+
+getWeight(Grid, Res_corners, Res_mobility, Res_coinParity, Res_stability, Res) :- 
+  count_xo_grid(Count, Grid),
+  Count > 50,
+  Res is 100 * Res_corners + 25 * Res_mobility + 50 * Res_coinParity + 10 * Res_stability.
+
+
+
+
+
+
+
+%% Dynamic evaluation
+dynamic_heuristic_evaluation_1st(Grid, MaxPlayer, MinPlayer, Res) :-
+  %stabilityHeuristic(Grid, MaxPlayer, MinPlayer, Res_stability),
+  %coinParityHeuristic(Grid, MaxPlayer, MinPlayer, Res_coinParity),
+  cornersCapturedHeuristic(Grid, MaxPlayer, MinPlayer, Res_corners),
+
+  %mobilityHeuristic(Grid, MaxPlayer, MinPlayer, Res_mobility),
+  Res_stability is 0,
+  Res_coinParity is 0,
+  %Res_corners is 0,
+  Res_mobility is 0,
+
+  getWeight(Grid, Res_corners, Res_mobility, Res_coinParity, Res_stability, Res).
+
+dynamic_heuristic_evaluation_2nd(Grid, MaxPlayer, MinPlayer, Res) :-
   stabilityHeuristic(Grid, MaxPlayer, MinPlayer, Res_stability),
   coinParityHeuristic(Grid, MaxPlayer, MinPlayer, Res_coinParity),
   cornersCapturedHeuristic(Grid, MaxPlayer, MinPlayer, Res_corners),
 
   mobilityHeuristic(Grid, MaxPlayer, MinPlayer, Res_mobility),
-  Res is 100 * Res_corners + 5 * Res_mobility + 25 * Res_coinParity + 25 * Res_stability.
+  getWeight2(Grid, Res_corners, Res_mobility, Res_coinParity, Res_stability, Res).
 
+getWeight2(_, Res_corners, Res_mobility, Res_coinParity, Res_stability, Res) :- 
+  Res is 100 * Res_corners + 5 * Res_mobility + 25 * Res_coinParity + 25 * Res_stability.
 % Res is 100 * Res_corners + 5 * Res_coinParity + 25 * Res_stability.
 
       % grilleDeDepart(Grid),
@@ -179,28 +259,5 @@ dynamic_heuristic_evaluation(Grid, MaxPlayer, MinPlayer, Res) :-
       % use_module(library(statistics)).
       % grilleDeDepart([L1|Rest]), Block = ["-", "-", "-", "-", "-", o, o, x],  GrilleBlock= [L1, L1, L1, L1, L1, Block, Block, Block],
       % profile(dynamic_heuristic_evaluation(GrilleBlock, x, o, R)).
-
-% *optimisation* since the dynamic_heuristic_evaluation is run on every move
-% (even if we have already compute the SAME grid before)
-% caching the Heuristic of a specific board can improve performance
-
-% get existing key
-get_or_compute_heuristic(Grid, MaxPlayer, MinPlayer, Res) :-
-  increment_stats_heuristic(Grid),
-  build_key([Grid, MaxPlayer, MinPlayer], Key),
-  get_cache(heuristic, Key, Res),
-  !.
-
-% check if the Heuristic has not been process for the other player
-get_or_compute_heuristic(Grid, MaxPlayer, MinPlayer, Res) :-
-  build_key([Grid, MinPlayer, MaxPlayer], Key),
-  get_cache(heuristic, Key, ResTMP),
-  Res is ResTMP * -1,!. % invert the result (since the Heuristic evaluation give the opposite)
-
-% compute Heuristic and store it
-get_or_compute_heuristic(Grid, MaxPlayer, MinPlayer, Res) :-
-  build_key([Grid, MaxPlayer, MinPlayer], Key),
-  dynamic_heuristic_evaluation(Grid, MaxPlayer, MinPlayer, Res),
-  set_cache(heuristic, Key, Res).
 
 % vim:set et sw=2 ts=2 ft=prolog:
